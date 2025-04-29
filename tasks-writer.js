baac94b63743b97c553a89cbba5e9bc49f483d7a
dmitriz <dmitri14@gmail.com>
2025-04-29 14:00:36 +0700

    refactor: exclude generated JS and declaration files from git tracking

--- tasks-writer.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { SETTINGS, FILES } from './config.js';
-const TASKS_FILE = path.join(process.cwd(), FILES.TASKS);
-export async function write_task(task) {
-    const newlines = '\n'.repeat(SETTINGS.NEWLINES_AFTER_TASK);
-    await fs.appendFile(TASKS_FILE, task + newlines, 'utf8');
-}
